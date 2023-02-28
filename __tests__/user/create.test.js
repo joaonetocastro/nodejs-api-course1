b8d93c134f21e7b07151ce8df251df693a1c3662
joaonetocastro <j.neto111@gmail.com>
2023-02-28 16:00:38 -0300

    test: post creation

--- __tests__/user/create.test.js
+++ __tests__/user/create.test.js
@@ -3,10 +3,19 @@ import supertest from 'supertest'
 // ...
 
 test("GET /products", async () => {
+  const product = {
+    name: 'any_product',
+    price: '98.72',
+    quantity: '12'
+  }
+  
 	await supertest(createServer())
-		.get("/products")
+		.post("/products")
+    .send(product)
 		.expect(200)
 		.then((response) => {
-			expect(Array.isArray(response.body)).toBeTruthy()
+			expect(response.body?.name).toBe('any_product')
+			expect(response.body?.price).toBe(98.72)
+			expect(response.body?.quantity).toBe(12)
 		})
 })
