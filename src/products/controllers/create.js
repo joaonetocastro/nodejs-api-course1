82949abb4e027fa775cfa93ad85d339dc19294a8
joaonetocastro <j.neto111@gmail.com>
2023-02-28 11:54:57 -0300

    feat: validation

--- src/products/controllers/create.js
+++ src/products/controllers/create.js
@@ -3,7 +3,9 @@ import {productsRepository} from '../repository'
 
 export const create = async (req, res) => {
     if(!req.body.name) throw new ValidationError('Name required')
-    
+    if(!req.body.price) throw new ValidationError('Price Required')
+    if(!req.body.quantity) throw new ValidationError('Quantity Required')
+
     const response = await productsRepository.create(req.body)
     
     res.send(response)
