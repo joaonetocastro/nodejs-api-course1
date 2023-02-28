e536706babbb516b1a9e4d8856243f0e90628758
joaonetocastro <j.neto111@gmail.com>
2023-02-28 11:56:33 -0300

    feat: prevent invalid field to be saved

--- src/products/controllers/create.js
+++ src/products/controllers/create.js
@@ -6,7 +6,11 @@ export const create = async (req, res) => {
     if(!req.body.price) throw new ValidationError('Price Required')
     if(!req.body.quantity) throw new ValidationError('Quantity Required')
 
-    const response = await productsRepository.create(req.body)
+    const response = await productsRepository.create({
+        name: req.body.name,
+        price: Number(Number(req.body.price).toFixed(2)),
+        quantity: Math.floor(Number(req.body.quantity)),
+    })
     
     res.send(response)
 }
