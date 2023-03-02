4d08da77a08015fff1942c5cde7b5633d2b63107
joaonetocastro <j.neto111@gmail.com>
2023-03-02 20:00:56 -0300

    feat: generate salts randomly

--- src/users/controllers/register.js
+++ src/users/controllers/register.js
@@ -11,8 +11,9 @@ export const register = async (req, res) => {
     if(userExists) {
         throw new ValidationError('User already registered.')
     }
-    
-    const passwordHash = await bcrypt.hash(req.body.password, 10)
+    const saltRound = 12
+    const salt = await bcrypt.genSalt(saltRound)
+    const passwordHash = await bcrypt.hash(req.body.password, salt)
 
     const response = await usersRepository.create({
         username: req.body.username,
